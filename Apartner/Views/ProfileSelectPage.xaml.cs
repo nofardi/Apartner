1e2c8b36454d22a4d5fa1cec2227282ddc031a3b
Diamant <ndiamant@paypal.com>
2018-07-08 12:13:51 +0300

    commit ignore

---
+++ Apartner/Views/ProfileSelectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Apartner.Views
+{
+    public partial class ProfileSelectPage : ContentPage
+    {
+        public ProfileSelectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
